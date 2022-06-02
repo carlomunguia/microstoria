7bd56d99f3248e340577622e2515c98fbae27105
Carlo Munguia <cmunguia@denimgroup.com>
2022-06-02 08:28:23 -0500

    Microstoria 8
    created gRPC data processing functions for microstoria service

---
+++ grpcapi/grpcapi.go
@@ -0,0 +1,32 @@
+package grpcapi
+
+import (
+	"database/sql"
+	"microstoria/mdb"
+	pb "microstoria/proto"
+	"time"
+)
+
+type MailServer struct {
+	pb.UnimplementedMicrostoriaServiceServer
+	db *sql.DB
+}
+
+func pbEntryToMdbEntry(pbEntry *pb.EmailEntry) mdb.EmailEntry {
+	t := time.Unix(pbEntry.ConfirmedAt, 0)
+	return mdb.EmailEntry{
+		Id:          pbEntry.Id,
+		Email:       pbEntry.Email,
+		ConfirmedAt: &t,
+		OptOut:      pbEntry.OptOut,
+	}
+}
+
+func mdbEntryToPbEntry(mdbEntry *mdb.EmailEntry) pb.EmailEntry {
+	return pb.EmailEntry{
+		Id:          mdbEntry.Id,
+		Email:       mdbEntry.Email,
+		ConfirmedAt: mdbEntry.ConfirmedAt.Unix(),
+		OptOut:      mdbEntry.OptOut,
+	}
+}
